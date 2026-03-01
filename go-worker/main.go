a19cafa5c61ec7b21ae62a19574d2dc2cb169cac
Arda Aras <cavduraras103@gmail.com>
2026-03-01 12:41:48 +0300

    feat: Add WebhookHandler

--- go-worker/main.go
+++ go-worker/main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"encoding/json"
+	"fmt"
+	"net/http"
 )
 
 type GitLabPayload struct {
 	ObjectKind string `json:"object_kind"`
-	ProjectID int `json:"project_id"`
+	ProjectID  int    `json:"project_id"`
 }
 
+func gitlabWebhookHandler(writing http.ResponseWriter, request *http.Request) {
+	if request.Method != http.MethodPost {
+		http.Error(writing, "Only POST Requests", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var payload GitLabPayload
+
+	json.NewDecoder(request.Body).Decode(&payload)
+
+	if payload.ObjectKind == "merge_request" {
+		fmt.Printf("Project ID: %d ", payload.ProjectID)
+	}
+}
